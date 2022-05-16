cc265faf13a0d7cb0043e122054a738f406c016a
ilham idrissi <ilhamidrissi2000@gmail.com>
2022-05-16 12:15:07 +0100

    Injection des dependances

---
+++ Seance 1/enset-ioc/src/dao/DaoImpl.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class DaoImpl implements IDao{
+    @Override
+    public double getData() {
+        /*
+         Se connecter à la BD pour récuperer la temperature
+         */
+        System.out.println("version base de donnees");
+        double temp=Math.random()*40;
+        return temp;
+    }
+}
